f5d4075db09b33c603cc3dfc0318663b55923d87
bdurushia <b.durushia@gmail.com>
2024-03-26 13:38:53 -0500

    Completed all exercises.

--- app.js
+++ app.js
@@ -3,8 +3,31 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1: Declaring Variables
+let lastName;
+var age;
 
-// Exercise 2
+// Exercise 2: Assigning Values to Variables
+const firstName = "Bryan";
+lastName = "Durushia";
+age = 29;
 
-// Exercise 3
+// Exercise 3: Declaring and Assigning Values to Variables
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4: Declaring and Assigning Values to Variables x2
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5: Declaring and Assigning Values to Variables x3
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
